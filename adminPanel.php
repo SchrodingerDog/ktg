1cbaec6ca6ee47ef374e8fda2e0c5c4e2448dd1b
Kamil Prosciewicz <kprosciewicz@gmail.com>
2013-10-21 18:13:07 +0200

    Zmiany w panelu admina, postep w dodawaniu trybu admina

--- adminPanel.php
+++ adminPanel.php
@@ -1,19 +1,18 @@
 <?php 
 
-
-
 if (!empty($_POST['tresc']) and !empty($_POST['tytul'])) {
 	$tytul = $_POST['tytul'];
 	$tresc = $_POST['tresc'];
 
-
-
 	dodajPost($tytul, $tresc);
+}
+if (isset($_POST['adminMode'])) {
+    adminMode();
 }
 
 function dodajPost($tytul, $tresc){
 	try{
-        $pdo = new PDO('mysql:host=mysql1.ugu.pl;dbname=db654524;charset=utf8', 'db654524', 'balbinka3');
+        $pdo = new PDO('mysql:host=localhost;dbname=ktg;charset=utf8', 'root', '');
         echo 'Połączono z bazą';
       }catch(PDOException $e){
         echo 'Ups, cos poszlo nie tak ';
@@ -25,10 +24,22 @@ function dodajPost($tytul, $tresc){
     }
 }
 
+function adminMode(){
+
+    echo "wlaczono tryb Admina";
+
+}
+
 ?>
 
 <form action = 'adminPanel.php' method="POST">
+
 Tytul postu: <input type="text" name="tytul"><br>
+
 Tresc postu: <textarea name="tresc"></textarea><br>
+
+<input type="checkbox" name="adminMode" id="checkbox" />
+<label for="checkbox"> Wlacz tryb Admina<br></label>
+
 <input type="submit" value="Submit">
 </form>
