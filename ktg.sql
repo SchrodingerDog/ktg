567ed2dc5e126895e68b0c65632b66b9ea195c75
Kamil Prosciewicz <kprosciewicz@gmail.com>
2013-11-03 16:36:08 +0100

    Plik bazy

--- ktg.sql
+++ ktg.sql
@@ -3,12 +3,18 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Czas wygenerowania: 31 Paź 2013, 21:54
--- Wersja serwera: 5.6.11
--- Wersja PHP: 5.5.3
+-- Czas wygenerowania: 03 Lis 2013, 16:34
+-- Wersja serwera: 5.5.32
+-- Wersja PHP: 5.4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Baza danych: `ktg`
@@ -33,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `members` (
   `login` text COLLATE latin1_general_cs NOT NULL,
   `thumb` text COLLATE latin1_general_cs NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs AUTO_INCREMENT=21 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs AUTO_INCREMENT=7 ;
 
 --
 -- Zrzut danych tabeli `members`
@@ -44,8 +50,8 @@ INSERT INTO `members` (`id`, `zdjecie`, `imie`, `nazwisko`, `opis`, `super_user`
 (2, 'member_photos\\blank_user.gif', 'Dorota', 'Tyralik', 'aaaaaa Plaseholder', 1, 'dortyr67', 'member_photos\\blank_user.gif'),
 (3, 'member_photos\\blank_user.gif', 'Adam', 'Kowalski', 'bbbbbbbbb', 0, '', 'member_photos\\blank_user.gif'),
 (4, 'member_photos\\911850_378291288968779_1616483127_n.jpg', 'Adam', 'Mickiewicz', 'testing testing', 0, '', 'member_photos\\blank_user.gif'),
-(17, 'member_photos\\Squirrel-nice-wallpaper.jpg', 'Wiewiórka', 'Ruda', 'Lol', 0, 'wierud104', 'member_photos\\thumbs\\Squirrel-nice-wallpaper.jpg'),
-(18, 'member_photos\\1187510-subzero_black_flash.jpg', 'Zero', 'Sub', 'Mróz', 0, 'zersub43', 'member_photos\\thumbs\\1187510-subzero_black_flash.jpg');
+(5, 'member_photos\\Squirrel-nice-wallpaper.jpg', 'Wiewiórka', 'Ruda', 'Lol', 0, 'wierud104', 'member_photos\\thumbs\\Squirrel-nice-wallpaper.jpg'),
+(6, 'member_photos\\1187510-subzero_black_flash.jpg', 'Zero', 'Sub', 'Mróz', 0, 'zersub43', 'member_photos\\thumbs\\1187510-subzero_black_flash.jpg');
 
 -- --------------------------------------------------------
 
@@ -67,30 +73,10 @@ CREATE TABLE IF NOT EXISTS `posts` (
 --
 
 INSERT INTO `posts` (`id`, `tytul`, `tresc`, `created`) VALUES
-(1, 'Tytul1', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n        
-  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n        
-  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n        
-  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n        
-  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n        
-  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2013-10-20'),
-(2, 'Tytul2', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n        
-  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n        
-  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n        
-  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n        
-  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n        
-  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2013-10-20'),
-(3, 'Tytul3', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n        
-  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n        
-  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n        
-  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n        
-  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n        
-  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2013-10-20'),
-(4, 'Tytul4', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n        
-  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n        
-  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n        
-  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n        
-  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n        
-  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2013-10-20'),
+(1, 'Tytul1', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n        \r\n  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n        \r\n  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n        \r\n  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n        \r\n  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n        \r\n  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2013-10-20'),
+(2, 'Tytul2', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n        \r\n  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n        \r\n  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n        \r\n  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n        \r\n  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n        \r\n  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2013-10-20'),
+(3, 'Tytul3', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n        \r\n  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n        \r\n  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n        \r\n  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n        \r\n  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n        \r\n  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2013-10-20'),
+(4, 'Tytul4', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n        \r\n  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n        \r\n  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n        \r\n  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n        \r\n  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n        \r\n  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2013-10-20'),
 (7, 'Tytul Paginacja', 'Tresc Paginacja', '2013-10-27'),
 (9, 'Tytul Paginacja', 'Tresc Paginacja', '2013-10-27'),
 (10, 'Tytul Paginacja', 'Tresc Paginacja', '2013-10-27'),
@@ -154,11 +140,11 @@ INSERT INTO `wyprawy_czlonkowie` (`wyprawa_id`, `czlonek_id`) VALUES
 (2, 1),
 (2, 4),
 (3, 2),
-(3, 3);
+(3, 3),
+(4, 1),
+(4, 5),
+(4, 6);
 
-
-SELECT `members`.`nazwisko`, `members`.`imie`, `wyprawy`.`cel`, `wyprawy`.`data` FROM
-`wyprawy`, `wyprawy_czlonkowie`, `members` WHERE
-`members`.`id`=`wyprawy_czlonkowie`.`czlonek_id` AND
-`wyprawy`.`id`=`wyprawy_czlonkowie`.`wyprawa_id` ORDER BY
-`members`.`nazwisko`
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
